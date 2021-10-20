bcaee78ab3add244f3f199dda350df26c24f3ba6
burggraf1 <jeffburggraf@gmail.com>
2021-10-20 00:17:05 -0700

    None

--- analysis/sandbox.py
+++ analysis/sandbox.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 import datetime
 import numpy as np
+from JSB_tools import mpl_hist
 from JSB_tools.spe_reader import SPEFile
 from cal_sources import CalSource
 from JSB_tools.nuke_data_tools.gamma_coince import Levels
@@ -25,17 +26,39 @@ from JSB_tools.nuke_data_tools.gamma_spec import gamma_search
 #     print(g)
 
 
-ax = None
-for i in range(129, 135):
-    shot = Shot(i)
-    label = shot.__repr__(['he_flow', 'ar_flow', 'shotnum'])
-    kwargs = {'time_max': 310, 'time_min': 0, 'remove_baseline': True, 'erg_min': 200, 'erg_max': 250}
-    if ax is None:
-        ax =shot.list.plot_erg_spectrum(**kwargs, label=label)
-        continue
+def _plot(l, plot_time=True, normalize=False):
+    fig_erg, ax_erg = plt.subplots()
+    if plot_time:
+        fig_time, ax_time = plt.subplots()
+    time_bins = np.arange(0, 250, 4)
 
-    shot.list.plot_erg_spectrum(ax=ax, **kwargs, label=label)
+    kwargs_ergs = {'time_max': 310, 'time_min': 0, 'remove_baseline': True, 'erg_min': 200, 'erg_max': 250}
+    kwargs_time = {'energy': 218.5, 'bins': time_bins}
 
+    for shot_num in l:
+        c = "red" if shot_num != 134 else 'blue'
+        shot = Shot(shot_num)
+        label = shot.__repr__(['he_flow', 'ar_flow', 'shotnum'])
+        y_erg, erg_bins = shot.list.get_erg_spectrum(**kwargs_ergs, return_bins=True)
+        if normalize:
+            y_erg /= np.max(y_erg)
+        mpl_hist(erg_bins, y_erg, label=label, ax=ax_erg, c=c)
+
+        if plot_time:
+            rates, _, _ = shot.list.get_time_dependence(**kwargs_time)
+            if normalize:
+                rates /= np.sum(rates)
+            mpl_hist(time_bins, rates, label=label, ax=ax_time)
+    if plot_time:
+        ax_time.set_xlabel("Time [s]")
+        ax_time.set_ylabel("Count rate [arb. units]")
+
+    ax_erg.set_xlabel("Energy")
+    ax_erg.set_ylabel("Normalized counts")
+
+_plot(list([120, 133, 129, 131, 134]),  plot_time=False, normalize=True)
+_plot(list([133, 129, 131, 134]),  plot_time=True, normalize=True)
+# _plot([98, 99, 129, 130])
 # Shot(i).list.plotly(remove_background=True)
 
 plt.show()
